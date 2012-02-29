e80a2b9b4212f81e0e99419609dfaee0e1ef97f9
Jonathan Varkovitzky <vark@uw.edu>
2012-02-29 11:16:16 -0800

    starting hw5 and storing grid files

---
+++ HW5/hw5.py
@@ -0,0 +1,8 @@
+#This program solves the 2D Euler Equations about the NACA-0012 airfoil
+#using the Jameson Finite Volume Method
+
+#Jonathan Varkovitzky
+#Feb 29, 2012
+
+from numpy import *
+from scipy import *
